053633b9f258b5cdb88949346e936590163c3cf1
Sandi Perdiansah <sandiperdiansah95@gmail.com>
2024-07-09 16:16:49 +0700

    add image and create section services

--- src/layouts/ServiveLayout.jsx
+++ src/layouts/ServiveLayout.jsx
@@ -1,9 +1,104 @@
-import {Flex} from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Stack, Text, VStack, Grid } from "@chakra-ui/react";
+import { data } from "../services/data.js";
 
-export default function ServiveLayout(){
-    return(
-        <Flex as='section' w='100%' h='100vh' alignItems='center' justifyContent='center'>
-            <h1>service</h1>
-        </Flex>
+export default function ServiceLayout() {
+    return (
+        <VStack
+            as='section'
+            w='100%'
+            alignItems='center'
+            role='region'
+            aria-labelledby='service-layout-heading'
+        >
+            <Flex
+                w='100%'
+                h={{ base: 'auto', lg: '100vh' }}
+                p={{ base: '2rem 1.4rem', lg: '2rem 4rem' }}
+                alignItems='center'
+                justifyContent='center'
+                flexDirection={{ base: 'column', lg: 'row' }}
+            >
+                <Heading
+                    as='h1'
+                    id='service-layout-heading'
+                    fontWeight='bold'
+                    fontSize={{ base: '2xl', lg: '4xl' }}
+                    color='primary'
+                    lineHeight={1.7}
+                    textAlign={{ base: 'center', lg: 'start' }}
+                    order={{ base: 2, lg: 1 }}
+                    data-aos='zoom-in'
+                    data-aos-duration='1000'
+                >
+                    Kami melayani berbagai jenis tugas mata kuliah dengan profesionalisme dan kualitas terbaik.
+                </Heading>
+                <Image
+                    boxSize={{ base: '15rem', lg: '24rem' }}
+                    src='/assets/images/services.svg'
+                    alt='Services Animation'
+                    aria-hidden='true'
+                    order={{ base: 1, lg: 2 }}
+                    data-aos='zoom-in'
+                    data-aos-duration='1000'
+                />
+            </Flex>
+
+            <ServiceList />
+        </VStack>
     )
+}
+
+const ServiceList = () => {
+    return (
+        <Box
+            w='100%'
+            p={{ base: '2rem 1.4rem', lg: '0 4rem 4rem' }}
+            mt={{ lg: '-6rem' }}
+            role='list'
+            aria-label='List of services'
+        >
+            <Text
+                mb='4'
+                color='font'
+                fontSize={{ base: '1rem', lg: 'lg' }}
+                lineHeight={1.7}
+                textAlign='center'
+                data-aos='zoom-in'
+                data-aos-duration='1000'
+            >
+                Berikut adalah beberapa layanan Tag Tugas yang kami tawarkan.
+            </Text>
+            <Grid
+                templateColumns={{ base: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
+                gap={4}
+                justifyContent='center'
+            >
+                {
+                    data.services.map((item) => (
+                        <Box
+                            key={item.id}
+                            p='4'
+                            borderWidth='1px'
+                            borderRadius='lg'
+                            textAlign='center'
+                            whiteSpace='nowrap'
+                            overflow='hidden'
+                            textOverflow='ellipsis'
+                            data-aos='zoom-in'
+                            data-aos-duration='1000'
+                            role='listitem'
+                            aria-label={`Service ${item.name}, Price Rp ${item.price}`}
+                        >
+                            <Stack spacing='3'>
+                                <Heading fontSize={{ base: 'xl', lg: '2xl' }}>{item.name}</Heading>
+                                <Text fontSize='xs' mt='-0.5rem'>
+                                    Rp {item.price}
+                                </Text>
+                            </Stack>
+                        </Box>
+                    ))
+                }
+            </Grid>
+        </Box>
+    );
 }
--- src/services/data.js
+++ src/services/data.js
@@ -1,24 +1,67 @@
 export const data = {
     navbar: [
         {
-            id:1,
+            id: 1,
             teks: 'Tentang Kami',
             path: 'tentang-kami'
         },
         {
-            id:2,
+            id: 2,
             teks: 'Layanan Kami',
             path: 'layanan-kami'
         },
         {
-            id:3,
+            id: 3,
             teks: 'Testimoni',
             path: 'testimoni'
         },
         {
-            id:4,
+            id: 4,
             teks: 'Cara Order',
             path: 'cara-order'
         }
     ],
+
+    services: [
+        {
+            id: 1,
+            name: 'Makalah',
+            price: '50k - 150k'
+        },
+        {
+            id: 2,
+            name: 'Jurnal',
+            price: '50k - 200k'
+        },
+        {
+            id: 3,
+            name: 'Resume',
+            price: '50k - 200k'
+        },
+        {
+            id: 4,
+            name: 'Skripsi',
+            price: '4jt - 6jt'
+        },
+        {
+            id: 5,
+            name: 'Thesis',
+            price: '10jt - 15jt'
+        },
+        {
+            id: 6,
+            name: 'Programming',
+            price: '100k - 1jt'
+        },
+        {
+            id: 7,
+            name: 'Desain',
+            price: '100k - 1jt'
+        },
+        {
+            id: 8,
+            name: 'dll',
+            price: '---'
+        },
+    ]
 }
